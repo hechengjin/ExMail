875866a49f1dea9ce181d9fe27d29b5657b845c7
hechengjin <he_chengjin@outlook.com>
2015-04-16 15:03:49 +0800

    增加缓存清理和打印日志的bat文件

---
+++ mail/app/startwithdel.bat
@@ -0,0 +1,22 @@
+if "%1"=="da" (
+	goto delall
+) else (
+	goto delini
+)
+
+:delall
+	set /p input="del all files?(y/n)"
+	if "%input%"=="y" (
+		rd /q /s "C:\Documents and Settings\hecj\Application Data\Thunderbird"
+	)
+	goto startup
+	
+:delini
+	for /d %%i in ("C:\Documents and Settings\hecj\Application Data\Thunderbird\Profiles\*") do (del "%%i\compatibility.ini")
+	del "C:\Documents and Settings\hecj\Application Data\Thunderbird\profiles.ini"
+	goto startup
+	
+:startup
+	E:\githubsrc\ExMail\obj_ExMail\mozilla\dist\bin\Thunderbird.exe
+
+:end
---
+++ mail/app/startwithlog.bat
@@ -0,0 +1,22 @@
+@echo off
+set MYLOG_DIR=log
+set curdir=%~dp0
+set logdir="%curdir%"%MYLOG_DIR%
+set NSPR_LOG_MODULES=smtp:5,imap:5,timestamp,sync
+set dataandtime=%date:~0,4%%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%
+set logfilename=.\%MYLOG_DIR%\%dataandtime%.log
+set NSPR_LOG_FILE=ExMail.log
+
+if not exist %logdir% (
+        md %logdir%
+) else (
+        echo dir ready.
+)
+
+if not exist %logdir% (
+        echo can't create ./log dir.
+        pause
+) else (
+        start .\ExMail.exe
+)
+exit
